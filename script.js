793979d0605cf2dda1a29458d4a6b18927ee779f
Arina <thearinamarr@gmail.com>
2018-06-04 18:05:28 +0700

    added main formulas and stuff

---
+++ script.js
@@ -0,0 +1,93 @@
+$(function(){
+
+    var excitation_current, torque, voltage, resistance = 0;
+
+    // реулятор для вольтметра
+    $('#v-control').knobKnob({
+        snap : 10,
+        value: 1,
+        turn : function (ratio) {
+         var deg = ratio * 90;
+         $('#v-spear').css('transform', 'rotate(' + (deg - 45) + 'deg)'); // поворачивает стрелку вольтметра
+
+         voltage = (ratio * 250).toFixed(1); // устанавливает значение напряжения
+         if (voltage == 0)
+           voltage = 0.1;
+           counting();
+         }
+    });
+
+
+    // реулятор для добавочного сопротивления
+    // $('.switch').on('click', function(){
+
+      // if ($('#switch1').prop('checked')) {
+
+       //  $('#res-control').knobKnob({
+       //     snap : 180,
+       //     value: 0,
+       //     });
+       // } else {
+         $('#res-control').knobKnob({
+           snap : 10,
+           value: 0,
+           turn :  function (ratio) {
+               resistance = (ratio * 3 + 1).toFixed(0);
+               counting();
+             }
+         });
+       // }
+    // });
+
+
+    // реулятор для амперметра
+    $('#a-control').knobKnob({
+        snap : 10,
+        value: 1,
+        turn : function (ratio) {
+         var deg = ratio * 90;
+         $('#a-spear').css('transform', 'rotate(' + (deg - 45) + 'deg)'); // поворачивает стрелку амперметра
+
+         excitation_current = (ratio * 5).toFixed(1); // устанавливает значение тока возбуждения
+         if (excitation_current == 0)
+           excitation_current = 0.1;
+           counting();
+         }
+    });
+
+
+    // регулятор момента сопротивления
+    $('#tor-control').knobKnob({
+        snap : 10,
+        value: 1,
+        turn : function (ratio) {
+          torque = (ratio * 20).toFixed(1);
+          new JSGadget.Display("#tor-display", {
+            digits: 3,
+            color: "white",
+            shadow: {color: "gray"}},
+            torque);
+            counting();
+        }
+    });
+
+
+    function counting(){
+
+      excitation_flux = excitation_current * 2;
+
+      var armature = torque / excitation_flux;
+      new JSGadget.Display("#arm-display", {
+        digits: 3,
+        color: "white",
+        shadow: {color: "gray"}},
+        armature);
+        // console.log('voltage / excitation_flux ' + (voltage / excitation_flux));
+      var angular = voltage / excitation_flux - (resistance + 2) / (excitation_flux * excitation_flux) * torque;
+      new JSGadget.Display("#ang-display", {
+        digits: 4,
+        color: "white",
+        shadow: {color: "gray"}},
+        angular.toFixed(1));
+    }
+});
