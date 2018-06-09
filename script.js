b67042307c5e5687762a4d9dcb38b1dc9836b648
Arina <thearinamarr@gmail.com>
2018-06-10 01:12:55 +0700

    all done.

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ $(function(){
 
 
     // реулятор для добавочного сопротивления
-    var counter = 0, last_resistance = 1; // TODO: почему-то при первом включении ставит не то значение
+    var counter = 0, last_resistance = 1;
     $('#res-control').knobKnob({
       snap : 10,
       value: 0,
@@ -27,16 +27,18 @@ $(function(){
           if ((counter % 2) != 0) {
             resistance = (ratio * 3 + 1).toFixed(0);
             counting();
+          } else {
+            last_resistance = (ratio * 3 + 1).toFixed(0);
           }
         }
     });
     $('.switch').on('click', function(){
       if ($('#switch1').prop('checked')) {
-        if ((counter % 2) != 0) {
+        if ((counter % 2) != 0) { // mode off
           last_resistance = resistance;
           resistance = 0;
           counting();
-        } else {
+        } else {                  // mode on
           resistance = last_resistance;
           counting();
         }
@@ -64,7 +66,7 @@ $(function(){
     // регулятор момента сопротивления
     $('#tor-control').knobKnob({
         snap : 1,
-        value: 1,
+        value: 89.5,
         turn : function (ratio) {
           torque = (ratio * 40 - 20).toFixed(1);
           new JSGadget.Display("#tor-display", {
@@ -83,17 +85,24 @@ $(function(){
 
       var armature = torque / excitation_flux;
       new JSGadget.Display("#arm-display", {
-        digits: 3,
-        color: "white",
-        shadow: {color: "gray"}},
-        armature);
-        // console.log('voltage / excitation_flux ' + (voltage / excitation_flux));
-      var angular = voltage / excitation_flux - (resistance + 2) / (excitation_flux * excitation_flux) * torque;
-      new JSGadget.Display("#ang-display", {
         digits: 4,
         color: "white",
         shadow: {color: "gray"}},
-        angular.toFixed(1));
+        armature.toFixed(1));
+      var angular = voltage / excitation_flux - (resistance + 2) / (excitation_flux * excitation_flux) * torque;
+      if ((angular < -99.99) || (angular > 999.99)) {
+        new JSGadget.Display("#ang-display", {
+          digits: 5,
+          color: "white",
+          shadow: {color: "gray"}},
+          angular.toFixed(1));
+      } else {
+        new JSGadget.Display("#ang-display", {
+          digits: 4,
+          color: "white",
+          shadow: {color: "gray"}},
+          angular.toFixed(1));
+      }
     }
 
     // power button
